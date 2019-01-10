f942a18af6a5a5472ba1602104d790223722ed94
AM-Scott <scottam3@mcmaster.ca>
2019-01-10 14:59:36 -0500

    Updated assignment

--- QMEE/Assignment_Week_1.R
+++ QMEE/Assignment_Week_1.R
@@ -10,7 +10,14 @@ str(soc.data)
 head(soc.data)
 
 # Calculate the mean Aggregation Index (AI) and standard deviation for the 2 treatments across generations (U=Up selection, D=Down selection)
-soc.data %>%
+summarized.soc.data <- soc.data %>%
   group_by(Generation, Treatment, Sex) %>%
   summarise(mean_AI = mean(AI),
             sd_AI = sd(AI))
+
+print(summarized.soc.data)
+
+# Just looking at the first and last generations to better compare the divergence at gen 8 to the starting means
+gens.1.and.8 <- filter(summarized.soc.data, Generation %in% c(1,8))
+
+print(gens.1.and.8)
