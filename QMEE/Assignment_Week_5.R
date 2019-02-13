94be57c297f6cd480582f4f40ee558dcf9f66d9b
AM-Scott <scottam3@mcmaster.ca>
2019-02-12 19:21:47 -0500

    Update assignment 5

--- QMEE/Assignment_Week_5.R
+++ QMEE/Assignment_Week_5.R
@@ -1,146 +1,111 @@
 ### Assignment Week 5 - Permutation tests
 
 library(tidyverse)
-library(lmPerm)
-library(coin)
-library(gtools)
+
+### Cleaning up/preparing data
 
 soc.data <- read_csv("data/Sociability_Raw_Data.csv")
 
-# make block, lineage, generation and sex factors
 soc.data <- soc.data %>%
   mutate_at(vars(Block,Lineage,Sex),factor)
 
-# Summarize counts for each observation (vars Q1-Q8) into a single aggregation index (AI) dependent variable
 soc.data <- soc.data %>%
   gather(key=Q,value=score,Q1:Q8) %>%
   group_by(Generation,Block,Dish,Lineage,Sex) %>%
   summarise(AI=var(score)/mean(score)) %>%
   full_join(soc.data)
 
-# Make a new factor "treatment" with 3 levels corresponding to direction of selection (U, D, C)
 soc.data <- soc.data %>%
-  ungroup() %>% # have to add this for it to work?
+  ungroup() %>%
   mutate(Treatment = as.factor(str_sub(Lineage, 1,1)))
 
+### Hyp 1a - In generation 1, any difference in mean social score between the Up/Down selection treatments would be expected by chance, as there has been no treatment-related artificial selection at this point.
+### Hyp 1b - In the latest generation (at this point, gen 9) selection should be causing the 2 treatments to become diverged, and differences at this point should not be expected by chance.
+### I can look at these hypotheses with permutation tests that scramble the response variable (social score) with respect to treatment.
+#
+### I would like to be able to look at these hypotheses separately for males and females.
+### Therefore, I'll make a generic function for the permutation test where the generation and sex being looked at can be easily modified.
 
 set.seed(999)
 
-### Hyp 1 - Would a difference between the Up/Down be expected by chance in gen 1? - just looking at males
-
-summarized.soc.data <- (soc.data
-               %>% group_by(Generation, Treatment, Sex)
-               %>% summarise(mean_AI = mean(AI))
-)
-
-# Observed mean difference in males
-g1.diff.M <- (summarized.soc.data
-              %>% filter(Generation == 1, Sex == "M", Treatment != "C")
+soc.selection.perm <- function(Gen, sex, data = soc.data, nsim = 2000) {
+  summarized.soc.data <- (data
+                        %>% group_by(Generation, Treatment, Sex)
+                        %>% summarise(mean_AI = mean(AI))
+                        )
+  # Observed mean difference
+  obs.diff <- (summarized.soc.data
+              %>% filter(Generation == Gen, Sex == sex, Treatment != "C")
               %>% pull(mean_AI)
               %>% diff()
-)
-print(g1.diff.M)
-
-# Permutation test
-gen1.M.data <- filter(soc.data, Generation == 1, Sex == "M", Treatment != "C")
-
-g1.M.result <- numeric(2000)
-for (i in 1:1999) {
-  bdat <- transform(gen1.M.data,
-                    AI = gen1.M.data$AI[sample(nrow(gen1.M.data))])
-  g1.M.result[i] <- (bdat
+              )
+  print(obs.diff, message("Observed Difference"))
+  # Permutation test
+  perm.data <- filter(soc.data, Generation == Gen, Sex == sex, Treatment != "C")
+  perm.result <- numeric(nsim)
+  for (i in 1:nsim) {
+    perm <- sample(nrow(perm.data))
+    pdat <- transform(perm.data,
+                      AI = AI[perm])
+    perm.result[i] <- (pdat
                      %>% group_by(Treatment)
                      %>% summarise(mean_AI = mean(AI))
                      %>% pull(mean_AI)
                      %>% diff
-  )
+    )
+    }
+  # add our actual observation to the result
+  perm.result <- c(perm.result, obs.diff)
+  
+  hist(perm.result, col="gray",main = "", breaks = 24)
+  abline(v=obs.diff, col = "red")
+  p.val <- mean(abs(perm.result)>=abs(obs.diff))
+  print(p.val, message("P-value"))
 }
-# add our actual observation as the 2000th
-g1.M.result[2000] <- g1.diff.M
 
-hist(g1.M.result, col="gray",main = "", breaks = 24)
-abline(v=g1.diff.M, col = "red")
+# Run the function separately for each generation and sex combination relating to the above hypotheses.
 
+soc.selection.perm(Gen = 1, sex = "F")
 
-### How does this difference look at gen 9? Also just in males
+soc.selection.perm(Gen = 9, sex = "F")
 
-# Observed mean difference in males
-g9.diff.M <- (summarized.soc.data
-              %>% filter(Generation == 9, Sex == "M", Treatment != "C")
-              %>% pull(mean_AI)
-              %>% diff()
-)
-print(g9.diff.M)
+soc.selection.perm(Gen = 1, sex = "M")
 
-# Permutation test
-gen9.M.data <- filter(soc.data, Generation == 9, Sex == "M", Treatment != "C")
-
-g9.M.result <- numeric(2000)
-for (i in 1:1999) {
-  bdat <- transform(gen9.M.data,
-                    AI = gen9.M.data$AI[sample(nrow(gen9.M.data))])
-  g9.M.result[i] <- (bdat
-                     %>% group_by(Treatment)
-                     %>% summarise(mean_AI = mean(AI))
-                     %>% pull(mean_AI)
-                     %>% diff
-  )
-}
-# add our actual observation as the 2000th
-g9.M.result[2000] <- g9.diff.M
-
-hist(g9.M.result, col="gray", main = "", breaks = 24)
-abline(v=g9.diff.M, col = "red")
-
-
-
-
-
-
-## females g9
-
-
-# Observed mean difference in females
-g9.diff.F <- (summarized.soc.data
-              %>% filter(Generation == 9, Sex == "F", Treatment != "C")
-              %>% pull(mean_AI)
-              %>% diff()
-)
-print(g9.diff.F)
-
-# Permutation test
-gen9.F.data <- filter(soc.data, Generation == 9, Sex == "F", Treatment != "C")
-
-g9.F.result <- numeric(2000)
-for (i in 1:1999) {
-  bdat <- transform(gen9.F.data,
-                    AI = gen9.F.data$AI[sample(nrow(gen9.F.data))])
-  g9.F.result[i] <- (bdat
-                     %>% group_by(Treatment)
-                     %>% summarise(mean_AI = mean(AI))
-                     %>% pull(mean_AI)
-                     %>% diff
-  )
-}
-# add our actual observation as the 2000th
-g9.F.result[2000] <- g9.diff.F
-
-hist(g9.F.result, col="gray", main = "", breaks = 24)
-abline(v=g9.diff.F, col = "red")
-
-
-
+soc.selection.perm(Gen = 9, sex = "M")
 
 #######################################################################
-#######################################################################
+### Preface: I'm not exactly sure if the following makes sense...
+### Hyp 2 - I expect that the treatment-by-generation interaction estimated via a linear model should not be expected by chance. 
+### (i.e. under the null hypothesis where the response variable (social score) is scrambled with respect to Treatment and Generation. 
+### This is because divergence between treatments should be increasing with generation.
 
-# lm at gen 9- females
+## For simplicity, I'll attempt this with just females
 
-gen9.F.data <- filter(soc.data, Generation == 9, Sex == "F", Treatment != "C")
+F.data <- filter(soc.data, Sex == "F", Treatment != "C")
 
-summary(lmp(AI ~ Treatment + Block + Dish, data = gen9.F.data))
+# Calculate the observed interaction estimate
+obs.inter <- coef(lm(AI ~ Treatment*Generation, data = F.data))[4]
+names(obs.inter) <- NULL # just want the number
 
-oneway_test(AI ~ Treatment, data = gen9.F.data)
+# Permutation
+nsim <- 2000
+perm.result.h2 <- numeric(nsim)
+for (i in 1:nsim) {
+  perm <- sample(nrow(F.data))
+  pdat <- transform(F.data,
+                    AI = AI[perm])
+  perm.inter <- coef(lm(AI ~ Treatment*Generation, data = pdat))[4]
+  names(perm.inter) <- NULL
+  perm.result.h2[i] <- perm.inter
+}
 
+# add our actual observation to the result
+perm.result.h2 <- c(perm.result.h2, obs.inter)
 
+hist(perm.result.h2, col="gray",main = "", breaks = 24)
+abline(v=obs.inter, col = "red")
+p.val <- mean(abs(perm.result.h2)>=abs(obs.inter))
+print(p.val)
 
+# p val is basically the same as what is given from the summary of the model:
+summary(lm(AI ~ Treatment*Generation, data = F.data))
---
+++ QMEE/Week6.R
@@ -0,0 +1,24 @@
+## Week 6
+
+## More permutation stuff
+
+# CONFIDENCE INTERVALS - can't have both permutation and confidence intervals 
+## PERMUTATION TESTS just say there is "something about the data" as permutation tests make no assumptions about the data or anything else
+
+# to get a confidence interval for the mean - have to assume the data are symmetric
+
+# Advantages and disadvantages
+
+# Nitrogen, phosporous and algae
+# Could imagine scrambling N, P, or both
+# Scramble the nitrogen - just  tells me that I can't trust what I see about nitrogen (null hypothesis of nitrogen doesn't matter)
+
+
+
+## Ben's part of the lecture
+
+# Actually coding the permutation
+
+# ****** other test statistics you can use - linear regression and take the slope
+
+# lmPerm computest the pvalues by permutation
