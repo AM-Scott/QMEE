b58207078f55743eced6e1f4ac642ee5e1c0b77c
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-03-12 13:55:03 -0400

    Marking (and moving a file whose name confused me)

--- QMEE/Assignment_Week_5.R
+++ QMEE/Assignment_Week_5.R
@@ -4,7 +4,8 @@ library(tidyverse)
 
 ### Cleaning up/preparing data
 
-soc.data <- read_csv("data/Sociability_Raw_Data.csv")
+## JD: Fixed directory case (try to match case for reproducibilty)
+soc.data <- read_csv("Data/Sociability_Raw_Data.csv")
 
 soc.data <- soc.data %>%
   mutate_at(vars(Block,Lineage,Sex),factor)
@@ -19,10 +20,14 @@ soc.data <- soc.data %>%
   ungroup() %>%
   mutate(Treatment = as.factor(str_sub(Lineage, 1,1)))
 
+### JD: Looking for absence of significance here is common, but probably misguided (although it's fine for this exercise)
+### A better way to control is to use interactions (treatment × generation), or to make a statistic for the difference.
 ### Hyp 1a - In generation 1, any difference in mean social score between the Up/Down selection treatments would be expected by chance, as there has been no treatment-related artificial selection at this point.
+
+### JD: This is not a scientific hypothesis at all. You're saying that real differences should be detectable (presumably because you have enough power?). Instead you should say what differences you're expecting.
 ### Hyp 1b - In the latest generation (at this point, gen 9) selection should be causing the 2 treatments to become diverged, and differences at this point should not be expected by chance.
 ### I can look at these hypotheses with permutation tests that scramble the response variable (social score) with respect to treatment.
-#
+
 ### I would like to be able to look at these hypotheses separately for males and females.
 ### Therefore, I'll make a generic function for the permutation test where the generation and sex being looked at can be easily modified.
 
@@ -73,8 +78,15 @@ soc.selection.perm(Gen = 1, sex = "M")
 
 soc.selection.perm(Gen = 9, sex = "M")
 
+## JD: It would be nice to see some explanation of these P values. 
+## What do you make of them? And why are they one-tailed?
+
+
 #######################################################################
+
 ### Preface: I'm not exactly sure if the following makes sense...
+### JD: Ha! Good that you're looking for an interaction.
+### Still not liking the way you phrase th hypothesis
 ### Hyp 2 - I expect that the treatment-by-generation interaction estimated via a linear model should not be expected by chance. 
 ### (i.e. under the null hypothesis where the response variable (social score) is scrambled with respect to Treatment and Generation. 
 ### This is because divergence between treatments should be increasing with generation.
@@ -109,3 +121,8 @@ print(p.val)
 
 # p val is basically the same as what is given from the summary of the model:
 summary(lm(AI ~ Treatment*Generation, data = F.data))
+
+## JD: Looking at the interaction coefficient as a statistic seems good (assuming you did it correctly), and it would have been good to explain that.
+## Again, it seems like you should be using two tails
+
+## JD: Grad 2/3 (good)
