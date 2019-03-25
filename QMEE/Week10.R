66970d366e954bc8cff6896219a95bc2c6abd67b
AM-Scott <scottam3@mcmaster.ca>
2019-03-25 10:17:49 -0400

    Update data

---
+++ QMEE/Week10.R
@@ -0,0 +1,22 @@
+### Week 10 - Power analysis
+
+### "magic power level" is 80%, some say 50%
+
+power.t.test(n=10, delta =2 , sd = 1)
+
+power.t.test(delta = 2, sd = 1, power = 0.8)
+
+### Mostly want to be solving for power or N with these (not the difference, or the variation)
+
+### Wednesday class - Dushoff returns to vitamin A example
+
+### - simulate proportion of false positives (if there were no effect, 
+### and you run the exp 1000 times, how often would you see an effect?)
+
+### All I need to go through with an experiment is the confidence interval (Is my
+### true value in the confidence interval, and does my confidence interval overlap 0)
+
+### cvr = coverage - want to know if you succeeded in getting the right answer
+
+### numvec... power curve (look at power as you increase the number of children (N))
+
